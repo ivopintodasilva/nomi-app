cd2ed21cb207f325759e14f4ec3b07377351d293
danifss <daniel.silva@ua.pt>
2015-10-19 17:28:49 +0100

    Added Activity profiles to manifest.

--- app/src/main/java/com/example/ivosilva/nomi/profiles/ProfileFragment.java
+++ app/src/main/java/com/example/ivosilva/nomi/profiles/ProfileFragment.java
@@ -1,10 +1,31 @@
 package com.example.ivosilva.nomi.profiles;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ivosilva.nomi.R;
 
 /**
  * Created by silva on 19-10-2015.
  */
 public class ProfileFragment extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        return view;
+    }
 }
