b684633aa69ee663eeb736b34e0404843fa9ca0c
danifss <daniel.silva@ua.pt>
2015-10-19 16:08:59 +0100

    Created Activity and layout for profiles.

---
+++ app/src/main/java/com/example/ivosilva/nomi/profiles/ProfileFragment.java
@@ -0,0 +1,10 @@
+package com.example.ivosilva.nomi.profiles;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by silva on 19-10-2015.
+ */
+public class ProfileFragment extends Fragment {
+
+}
