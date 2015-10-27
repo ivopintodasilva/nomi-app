1e2fb256d6d7a815cfea569492786d16712acafb
danifss <daniel.silva@ua.pt>
2015-10-27 02:44:50 +0000

    Added a Dialog to change an attribute. Just number for now.

--- app/src/main/java/com/example/ivosilva/nomi/profiles/ProfileDetailsFragment.java
+++ app/src/main/java/com/example/ivosilva/nomi/profiles/ProfileDetailsFragment.java
@@ -1,11 +1,15 @@
 package com.example.ivosilva.nomi.profiles;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.ivosilva.nomi.R;
@@ -47,14 +51,14 @@ public class ProfileDetailsFragment extends Fragment {
 
             JSONObject contacts = new JSONObject(getArguments().getString("ATTRIBUTES", ""));
 
-            /*  to organize the contacts in the view  */
+            /*  to organize the profile contacts in the view  */
             int i = 0;
             String key;
             Iterator<String> it = contacts.keys();
             while(it.hasNext()){
                 key = it.next();
                 Log.d("KEYS", key);
-                Log.d("i", Integer.toString(i));
+//                Log.d("i", Integer.toString(i));
 
                 switch (key){
                     case "NUMBER":
@@ -67,6 +71,7 @@ public class ProfileDetailsFragment extends Fragment {
                             params.topMargin = i*120;
                             number.setLayoutParams(params);
                         }
+                        number.setOnClickListener(numberHandler);
                         break;
                     case "EMAIL":
                         IconTextView email = (IconTextView) view.findViewById(R.id.profile_email);
@@ -77,7 +82,6 @@ public class ProfileDetailsFragment extends Fragment {
                             ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) email.getLayoutParams();
                             params.topMargin = i*120;
                             email.setLayoutParams(params);
-
                         }
                         break;
                     case "FACEBOOK":
@@ -145,4 +149,39 @@ public class ProfileDetailsFragment extends Fragment {
 
         return view;
     }
+
+    View.OnClickListener numberHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//            builder.setTitle(getContext().getString(R.string.attribute_dialog_title));
+
+            // Get the layout inflater
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.dialog_profile_detail_edit, null))
+                // Add action buttons
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.i("DIALOG","YES");
+                        /// TODO making change the value
+                    }
+                })
+                .setNeutralButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.i("DIALOG", "CANCEL");
+                        dialog.cancel();
+                    }
+                });
+
+            builder.create();
+//            alert.getButton(DialogInterface.BUTTON_POSITIVE).setBackgroundColor(getContext().getColor(R.color.colorPrimaryDark));
+//            alert.getButton(DialogInterface.BUTTON_NEUTRAL).setBackgroundColor(getContext().getColor(R.color.colorPrimaryDark));
+
+            builder.show();
+        }
+    };
+
 }
