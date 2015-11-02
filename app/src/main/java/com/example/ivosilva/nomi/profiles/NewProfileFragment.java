e19bdc981fcfd3df02b2af27ec02bc0558ce0c88
danifss <daniel.silva@ua.pt>
2015-11-02 21:45:58 +0000

    Just for testing.

--- app/src/main/java/com/example/ivosilva/nomi/profiles/NewProfileFragment.java
+++ app/src/main/java/com/example/ivosilva/nomi/profiles/NewProfileFragment.java
@@ -4,12 +4,14 @@ package com.example.ivosilva.nomi.profiles;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.ivosilva.nomi.R;
 import com.joanzapata.iconify.Iconify;
@@ -17,6 +19,10 @@ import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
+import mehdi.sakout.fancybuttons.FancyButton;
 
 /**
  * Created by silva on 30-10-2015.
@@ -53,6 +59,19 @@ public class NewProfileFragment extends Fragment {
         adapter.setDropDownViewResource(R.layout.spinner_nfc_list);
         lstColors.setAdapter(adapter);
 
+
+        final FancyButton btnCreateProfile = (FancyButton) view.findViewById(R.id.btn_create_new_profile);
+        btnCreateProfile.setOnClickListener(btnCreateProfileHandler);
+
         return view;
     }
+
+    View.OnClickListener btnCreateProfileHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            Log.i("NEWPROFILE", "Created new Profile");
+
+            ///TODO create new profile
+            Toast.makeText(getContext(), "Criaste um puto dum profile!", Toast.LENGTH_LONG).show();
+        }
+    };
 }
