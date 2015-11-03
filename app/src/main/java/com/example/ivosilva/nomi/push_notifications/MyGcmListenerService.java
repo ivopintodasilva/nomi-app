b849355e396dc70fd641d354bdede24833bcec81
ivopintodasilva <ivopintodasilva@gmail.com>
2015-11-03 02:35:17 +0000

    gcm registration almost working

---
+++ app/src/main/java/com/example/ivosilva/nomi/push_notifications/MyGcmListenerService.java
@@ -0,0 +1,32 @@
+package com.example.ivosilva.nomi.push_notifications;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+public class MyGcmListenerService extends GcmListenerService {
+
+    private static final String TAG = "MyGcmListenerService";
+
+
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+        Log.d(TAG, "From: " + from);
+        Log.d(TAG, "Message: " + message);
+
+
+    }
+}
