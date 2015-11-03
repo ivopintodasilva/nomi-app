1d40e0f5919ac1970e8fc6c2eb2db7abb28d313f
ivopintodasilva <ivopintodasilva@gmail.com>
2015-11-03 18:57:46 +0000

    notifications are being created

--- app/src/main/java/com/example/ivosilva/nomi/push_notifications/MyGcmListenerService.java
+++ app/src/main/java/com/example/ivosilva/nomi/push_notifications/MyGcmListenerService.java
@@ -1,18 +1,23 @@
 package com.example.ivosilva.nomi.push_notifications;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.content.Intent;
 import android.content.Context;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
+import com.example.ivosilva.nomi.MainActivity;
+import com.example.ivosilva.nomi.R;
 import com.google.android.gms.gcm.GcmListenerService;
 
 public class MyGcmListenerService extends GcmListenerService {
@@ -24,9 +29,22 @@ public class MyGcmListenerService extends GcmListenerService {
     @Override
     public void onMessageReceived(String from, Bundle data) {
         String message = data.getString("message");
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.gito)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.gito))
+                        .setContentTitle("My Notification Title")
+                        .setContentText("Something interesting happened");
+        int NOTIFICATION_ID = 12345;
+
+        Intent targetIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, targetIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+        NotificationManager nManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nManager.notify(NOTIFICATION_ID, builder.build());
+
         Log.d(TAG, "From: " + from);
         Log.d(TAG, "Message: " + message);
-
-
     }
 }
